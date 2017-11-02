8e570cff655ad0c733491f0ae784574c61924d7a
Anthony Leto <anthonyleto@mac.com>
2017-11-02 14:53:40 +0000

    Added support for SLES 12
    Signed-off-by: Anthony Leto <anthonyleto@mac.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -41,6 +41,19 @@ class tuned::params {
           $active_profile_conf = 'active_profile'
         }
 
+    'SLES': {
+      case $::operatingsystemmajrelease {
+        '12': {
+          $majversion = pick($_majversion, '2')
+          $dynamic_tuning = true
+          $main_conf = '/etc/tuned/tuned-main.conf'
+          $services = ['tuned']
+          $profile = '' #autodetect
+          $profiles_path = '/etc/tuned'
+          $active_profile_conf = 'active_profile'
+        }
+    }
+
         default: {
           fail("Unsupported OS release: \
 ${::operatingsystem} ${::operatingsystemmajrelease}")
